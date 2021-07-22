d9da65568bcb89938f76752d6f733cb2047e2896
prent7th <pc700f@gmail.com>
2021-07-22 07:56:19 +0000

    Add AllFile

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+cp /home/isucon/isucon7_prac/nginx.conf /etc/nginx/sites-available/nginx.conf
+cd /home/isucon/isucon7_prac/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+cd /home/isucon/isucon7_prac/app.py /home/isucon/isubata/webapp/python/app.py
+cd /home/isucon/isucon7_prac/env.sh /home/isucon/env.sh
